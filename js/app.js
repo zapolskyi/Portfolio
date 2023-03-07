e8bc28d60ae5f1e7c5faf0cf610a731ab8378422
zapolskyi <113490649+zapolskyi@users.noreply.github.com>
2023-03-07 18:49:24 +0200

    test5
    test5

--- js/app.js
+++ js/app.js
@@ -119,8 +119,25 @@ $(function () {
         nav.toggleClass("show");
     });
 
-
-
-
 });
+
+//Header-Shadow//
+
+let header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+    let scrollPos = window.scrollY;
+
+    if(scrollPos > 0) {
+        header.classList.add('shadow');
+    } else {
+        header.classList.remove('shadow');
+    }
+
+
+    console.log (scrollPos);
+
+})
+
+console.log (header);
 
